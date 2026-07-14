25c58966ed49f71d48ba99c43163dc7a0b2a872a
albertomaydayjhondoe <flousoloflou@gmail.com>
2026-07-14 17:32:49 +0000

    biuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   base: "/lagrange-lab-1/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
